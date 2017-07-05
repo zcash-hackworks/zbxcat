3f39382b5dba33595a3b57b77bff0adc37dfc889
Jay Graber <jay@z.cash>
2017-07-05 13:35:20 -0700

    Refactor test.py into unittest framework

--- test.py
+++ test.py
@@ -1,17 +1,27 @@
+import unittest
 from api import *
+from utils import *
 
+class SimpleTestCase(unittest.TestCase):
+    def setUp(self):
+        print("Starting testing 123")
+        self.htlcTrade = initiate()
 
-print("Starting test of xcat...")
-htlcTrade = initiate()
-fund_buyer()
-# zXcat.generate(8)
+class TestRedeem(SimpleTestCase):
+    def testfundBuyer(self):
+        fund_buyer()
+        # zXcat.generate(8)
+        zXcat.generate(6)
+        redeem_seller()
+        zXcat.generate(2)
+        bXcat.generate(20)
+        redeem_buyer()
+        trade = get_trade()
+        print("sellContract redeem_tx is:", trade.sellContract.redeem_tx)
+        self.assertEqual(trade.sellContract.redeem_tx, False)
 
-zXcat.generate(6)
-redeem_seller()
-zXcat.generate(2)
-bXcat.generate(20)
-redeem_buyer()
-
+if __name__ == '__main__':
+    unittest.main()
 
 # addr = CBitcoinAddress('tmFRXyju7ANM7A9mg75ZjyhFW1UJEhUPwfQ')
 # print(addr)
