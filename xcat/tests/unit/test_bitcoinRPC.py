e13288f17e506483feefa7a1c7e6e0a50e64d9dd
James Prestwich <james@prestwi.ch>
2017-09-15 16:21:38 -0600

    placeholder tests for bitcoinRPC

--- xcat/tests/unit/test_bitcoinRPC.py
+++ xcat/tests/unit/test_bitcoinRPC.py
@@ -42,6 +42,14 @@ class TestBitcoinProxy(unittest.TestCase):
             'Allowed networks are regtest, testnet, mainnet.'
             in str(context.exception))
 
+        with self.assertRaises(ValueError) as context_two:
+            proxy = bitcoinProxy(network=819.3)
+            self.assertIsNone(proxy)
+
+        self.assertTrue(
+            'Allowed networks are regtest, testnet, mainnet.'
+            in str(context_two.exception))
+
     def test_init_with_invalid_timeout(self, mock_rpc):
         """Test bitcoinProxy.__init__"""
 
@@ -60,3 +68,66 @@ class TestBitcoinProxy(unittest.TestCase):
         self.assertTrue(
             'Timeout should be a positive integer.'
             in str(context_two.exception))
+
+    def test_validateaddress(self, mock_rpc):
+        pass
+
+    def test_find_secret(self, mock_rpc):
+        pass
+
+    def test_parse_secret(self, mock_rpc):
+        pass
+
+    def test_get_keys(self, mock_rpc):
+        pass
+
+    def test_privkey(self, mock_rpc):
+        pass
+
+    def test_hashtimelockcontract(self, mock_rpc):
+        pass
+
+    def test_fund_htlc(self, mock_rpc):
+        pass
+
+    def test_check_funds(self, mock_rpc):
+        pass
+
+    def test_get_fund_status(self, mock_rpc):
+        pass
+
+    def test_search_p2sh(self, mock_rpc):
+        pass
+
+    def test_get_tx_details(self, mock_rpc):
+        pass
+
+    def test_redeem_contract(self, mock_rpc):
+        pass
+
+    def test_redeem(self, mock_rpc):
+        pass
+
+    def test_refund(self, mock_rpc):
+        pass
+
+    def test_parse_script(self, mock_rpc):
+        pass
+
+    def test_find_redeemblocknum(self, mock_rpc):
+        pass
+
+    def test_find_redeemAddr(self, mock_rpc):
+        pass
+
+    def test_find_refundAddr(self, mock_rpc):
+        pass
+
+    def test_find_transaction_to_address(self, mock_rpc):
+        pass
+
+    def test_new_bitcoin_addr(self, mock_rpc):
+        pass
+
+    def test_generate(self, mock_rpc):
+        pass
